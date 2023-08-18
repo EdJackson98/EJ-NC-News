22871a75fc62edf9559f2255b042aab24b406d8b
Ed <erj@sky.com>
2023-08-18 10:48:00 +0100

    tidying up articles model

--- models/articles.model.js
+++ models/articles.model.js
@@ -41,10 +41,7 @@ exports.checkIDExists = (article_id) => {
     });
 };
 
-exports.fetchAllArticles = (queryParams) => {
-  let {
-    
-  } = queryParams
+exports.fetchAllArticles = () => {
   let query =
     `SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.article_id) AS comment_count
     FROM articles 
